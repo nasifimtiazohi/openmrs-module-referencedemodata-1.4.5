1d816478cb5ca275b266e1f6d37e895769395123
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-09-06 00:15:12 -0400

    setting up specific release repo

---
+++ api/src/main/java/org/openmrs/module/referencedemodata/ReferenceDemoDataConstants.java
@@ -0,0 +1,44 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.referencedemodata;
+
+public class ReferenceDemoDataConstants {
+
+	public static final String CLERK_PERSON_UUID = "007037a0-0500-11e3-8ffd-0800200c9a66";
+	public static final String NURSE_PERSON_UUID = "9bed23d0-0502-11e3-8ffd-0800200c9a66";
+	public static final String DOCTOR_PERSON_UUID = "af7c3340-0503-11e3-8ffd-0800200c9a66";
+    public static final String SYSADMIN_PERSON_UUID = "b7009090-4015-11e4-8e9b-2939a1809c8e";
+	public static final String CLERK_PROVIDER_UUID = "043d8490-0506-11e3-8ffd-0800200c9a66";
+	public static final String NURSE_PROVIDER_UUID = "bfee3950-0506-11e3-8ffd-0800200c9a66";
+	public static final String DOCTOR_PROVIDER_UUID = "e9489e80-0506-11e3-8ffd-0800200c9a66";
+	public static final String CLERK_USER_UUID = "4882dd60-0508-11e3-8ffd-0800200c9a66";
+	public static final String NURSE_USER_UUID = "564b2790-0508-11e3-8ffd-0800200c9a66";
+	public static final String DOCTOR_USER_UUID = "67bfd7f0-0508-11e3-8ffd-0800200c9a66";
+	public static final String SYSADMIN_USER_UUID = "8295e796-4016-11e4-8e9b-2939a1809c8e";
+	public static final String NURSE_ROLE = "Organizational: Nurse";
+	public static final String CLERK_ROLE = "Organizational: Registration Clerk";
+	public static final String DOCTOR_ROLE = "Organizational: Doctor";
+    public static final String SYSADMIN_ROLE = "Organizational: System Administrator";
+    public static final String CLERK_PROVIDER_ROLE_UUID = "87d2122b-691c-4e06-9366-111f5ab2f526";
+	public static final String NURSE_PROVIDER_ROLE_UUID = "da13814f-f560-46df-8bb2-219e146c2811";
+	public static final String DOCTOR_PROVIDER_ROLE_UUID = "c110f9bc-c65f-44a2-a028-2af7e8fff534";
+    public static final String SCHEDULER_USERNAME = "scheduler";
+    public static final String SCHEDULER_PASSWORD = "Scheduler123";
+    public static final String SCHEDULER_PERSON_UUID = "14410ae4-2174-11e3-8ad1-1b02b898d14d";
+    public static final String SCHEDULER_USER_UUID = "0a3493e8-21f8-11e3-8ad1-1b02b898d14d";
+	public static final String CREATE_DEMO_PATIENTS_ON_NEXT_STARTUP = "referencedemodata.createDemoPatientsOnNextStartup";
+	public static final String VITALS_FORM_UUID = "a000cb34-9ec1-4344-a1c8-f692232f6edd";
+	public static final String VITALS_FORM_ENCOUNTERTYPE_UUID = "67a71486-1a54-468f-ac3e-7091a9a79584";
+	public static final String VITALS_FORM_NAME = "Vitals";
+}
---
+++ omod/src/test/java/org/openmrs/module/referencedemodata/ReferenceDemoDataActivatorTest.java
@@ -0,0 +1,194 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.referencedemodata;
+
+import org.hibernate.cfg.Environment;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.openmrs.GlobalProperty;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.User;
+import org.openmrs.Visit;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.LocationService;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.ProviderService;
+import org.openmrs.api.UserService;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.idgen.SequentialIdentifierGenerator;
+import org.openmrs.module.idgen.service.IdentifierSourceService;
+import org.openmrs.module.idgen.validator.LuhnMod30IdentifierValidator;
+import org.openmrs.module.referencemetadata.ReferenceMetadataActivator;
+import org.openmrs.module.referencemetadata.ReferenceMetadataConstants;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.SkipBaseSetup;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@SkipBaseSetup
+public class ReferenceDemoDataActivatorTest extends BaseModuleContextSensitiveTest {
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	ProviderService providerService;
+
+    @Autowired
+    AdministrationService adminService;
+
+    @Autowired
+    PatientService patientService;
+    
+    @Autowired
+    VisitService visitService;
+    
+	/**
+	 * @see ReferenceDemoDataActivator#started()
+	 * @verifies install the metadata package on startup
+	 */
+	@Test
+	public void started_shouldInstallTheMetadataPackageOnStartup() throws Exception {
+		initializeInMemoryDatabase();
+		executeDataSet("requiredDataTestDataset.xml");
+		authenticate();
+
+		new ReferenceMetadataActivator().started();
+
+		LocationService ls = Context.getLocationService();
+		final int initialLocationCount = ls.getAllLocations().size();
+		
+		new ReferenceDemoDataActivator().started();
+		
+		assertEquals(initialLocationCount + 7, ls.getAllLocations().size());
+
+		assertThat(userService.getUserByUsername("clerk"), is(notNullValue()));
+		assertThat(userService.getUserByUsername("nurse"), is(notNullValue()));
+		assertThat(userService.getUserByUsername("doctor"), is(notNullValue()));
+
+		assertThat(providerService.getProviderByIdentifier("clerk"), is(notNullValue()));
+		assertThat(providerService.getProviderByIdentifier("nurse"), is(notNullValue()));
+		assertThat(providerService.getProviderByIdentifier("doctor"), is(notNullValue()));
+	}
+
+    /**
+     * @verifies create a scheduler user and set the related global properties
+     * @see ReferenceDemoDataActivator#started()
+     */
+    @Test
+    public void started_shouldCreateASchedulerUserAndSetTheRelatedGlobalProperties() throws Exception {
+        initializeInMemoryDatabase();
+        executeDataSet("requiredDataTestDataset.xml");
+        authenticate();
+
+        new ReferenceMetadataActivator().started();
+
+        User user = userService.getUserByUsername(ReferenceDemoDataConstants.SCHEDULER_USERNAME);
+        assertNull(user);
+
+        new ReferenceDemoDataActivator().started();
+
+        user = userService.getUserByUsername(ReferenceDemoDataConstants.SCHEDULER_USERNAME);
+        assertNotNull(user);
+        assertTrue(user.isSuperUser());
+        assertEquals(ReferenceDemoDataConstants.SCHEDULER_USERNAME, adminService.getGlobalProperty("scheduler.username"));
+        assertEquals("Scheduler123", adminService.getGlobalProperty("scheduler.password"));
+    }
+    
+    // See https://groups.google.com/a/openmrs.org/forum/#!searchin/dev/Timeout$20trying$20to$20lock$20table/dev/rcQJCWvN8DQ/mp2_i7YGm8cJ
+    @Override
+    public Properties getRuntimeProperties() {
+        Properties props = super.getRuntimeProperties();
+        String url = props.getProperty(Environment.URL);
+        if (url.contains("jdbc:h2:") && !url.contains(";MVCC=TRUE")) {
+            props.setProperty(Environment.URL, url + ";MVCC=true");
+        }
+        return props;
+    }
+    
+    /**
+     * @verifies that some demo patients are created on startup
+     * @see ReferenceDemoDataActivator#started()
+     */
+    @Test
+    public void started_shouldCreateDemoPatients() throws Exception {
+    	final int demoPatientCount = 10;
+        initializeInMemoryDatabase();
+        executeDataSet("requiredDataTestDataset.xml");
+        authenticate();
+
+        GlobalProperty createDemoPatients = new GlobalProperty(ReferenceDemoDataConstants.CREATE_DEMO_PATIENTS_ON_NEXT_STARTUP, ""+demoPatientCount);
+        adminService.saveGlobalProperty(createDemoPatients);
+        
+        new ReferenceMetadataActivator().started();
+        
+		ReferenceDemoDataActivator referenceDemoDataActivator = new ReferenceDemoDataActivator();
+        initMockGenerator(referenceDemoDataActivator);
+		referenceDemoDataActivator.started();
+
+		List<Patient> allPatients = patientService.getAllPatients();
+        for (Patient patient : allPatients) {
+	        System.out.println(patient + " " + patient.getPatientIdentifier() + " " + patient.getPersonName() + " " + patient.getBirthdate() + " " + patient.getAddresses());
+        }
+		assertEquals(demoPatientCount, allPatients.size());
+        List<Visit> allVisits = visitService.getAllVisits();
+        for (Visit visit : allVisits) {
+	        System.out.println(visit + " " + visit.getStartDatetime() + " - " + visit.getStopDatetime() + " " + visit.getEncounters());
+        }
+		assertTrue(allVisits.size() > demoPatientCount);
+    }
+    
+    long seed = 0;
+    SequentialIdentifierGenerator mockIdGenerator;
+
+    private void initMockGenerator(ReferenceDemoDataActivator referenceDemoDataActivator) {
+		PatientIdentifierType openmrsIdType = patientService.getPatientIdentifierTypeByName(ReferenceMetadataConstants.OPENMRS_ID_NAME);
+    	mockIdGenerator = new SequentialIdentifierGenerator();
+    	mockIdGenerator.setIdentifierType(openmrsIdType);
+    	mockIdGenerator.setName(ReferenceMetadataConstants.OPENMRS_ID_GENERATOR_NAME);
+    	mockIdGenerator.setUuid(ReferenceMetadataConstants.OPENMRS_ID_GENERATOR_UUID);
+    	mockIdGenerator.setBaseCharacterSet(new LuhnMod30IdentifierValidator().getBaseCharacters());
+    	mockIdGenerator.setMinLength(6);
+    	mockIdGenerator.setFirstIdentifierBase("10000");
+
+        IdentifierSourceService mockIss = Mockito.mock(IdentifierSourceService.class);
+        Mockito.when(mockIss.generateIdentifier(Mockito.eq(openmrsIdType), Mockito.eq("DemoData"))).thenAnswer(new Answer<String>() {
+        	@Override
+        	public String answer(InvocationOnMock invocation) throws Throwable {
+        	    return generateIdentifier();
+        	}
+        });
+        referenceDemoDataActivator.setIdentifierSourceService(mockIss);
+    }
+    
+    private String generateIdentifier() {
+    	seed++;
+	    return mockIdGenerator.getIdentifierForSeed(seed);
+    }
+    
+}
